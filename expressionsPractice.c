3bba5eb550c715ee84affac1be24ee8a0b40bc2f
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-09-12 02:35:16 +0000

    tried to make the code, need help to make it work

--- expressionsPractice.c
+++ expressionsPractice.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 
 int main(void){
-    int x = 7, y = 24, z = 8, a = 4, s = 6, d = 18, c = 3, f = 2, v = 5, g = 
-    printf("%d", (int)x - (int)y / (int)z * (int)a + (int)s);
+    int a = 7-24/8*4+6;
+
+    int b = 18/3-7+2*5;
+
+    int c = 6*4/12+72/8-9;
+
+    int d = (17-6/2)+4*3;
+
+    int e = -2*(1*4-2/2)+(6+2-3);
+
+    int f = -1*5/(3-4*7)-2*24/6;
+
+    int g = (3*5*5/15)-(5-2*2);
+    
+    int h = (1*1*1*1*2*2+3*3*3)-2*2*2*2*2/4;
+
+    int i = (22*22/2*2-2*2*5*5);
+   
+    printf("%f", (float)a);
+
+    printf("%f", (float)b);
+
+    printf("%f", (float)c);
+
+    printf("%f", (float)d);
+
+    printf("%f", (float)e);
+
+    printf("%f", (float)f);
+
+    printf("%f", (float)g);
+
+    printf("%f", (float)h);
+
+    printf("%f", (float)i);
     return 0; 
 }
-// turn every variable into an equation.
+// turn every variable into an entire equation to simplify the code.
