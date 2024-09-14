6f5baac37c6ef769f6a952f80a0551e86a25d210
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-09-14 04:31:08 +0000

    finished budgetCalculator.c probably

--- budgetCalculator.c
+++ budgetCalculator.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 
 int main(void){
-    char income[9];
-    char utilities[9];
-    char groceries[9];
-    char car[9];
+    float income, rent, utils, groceries, transportation, savings, expenses, spend;
+    float prent, putils, pgroceries, ptransport, psavings, pexpenses;
     printf("This is a budget calculator.\n What is your income per month?\n");
-    scanf("%s", income);
+    scanf("%f", &income);
+    printf("how much does your rent cost?\n");
+    scanf("%f", &rent);
     printf("how much do your utilities cost?\n");
-    scanf("%s", income);
-    printf("You make %s\n", income);
-    float budget[9] = (float) income - (float) utilities - (float) groceries - (float) car;
-    printf("%f", budget);
+    scanf("%f", &utils);
+    printf("how much do your groceries cost?\n");
+    scanf("%f", &groceries);
+    printf("how much do your transportation cost?\n");
+    scanf("%f", &transportation);
+    savings = income * .2;
+    expenses = rent + utils + groceries + transportation;
+    spend = income - expenses - savings;
+    printf("you make $%.2f\n", income);
+    printf("your expenses are $%.f\n", expenses);
+    printf("your spending money is $%.2f\n", spend);
+    printf("%.2f\n", income);
+    prent = rent/income *100;
+    putils = utils/income *100;
+    pgroceries = groceries/income *100;
+    ptransport = transportation/income *100;
+    psavings = savings/income *100; 
+    printf("your rent is %d%% of your income\n", (int)prent);
+    printf("your utilities is %d%% of your income\n", (int)putils);
+    printf("your groceries is %d%% of your income\n", (int)pgroceries);
+    printf("your transportation costs are %d%% of your income\n", (int)transportation);
+    printf("your savings is %d%% of your income\n", (int)psavings);
     return 0;
 }
--- budgetCalculator.py
+++ budgetCalculator.py
@@ -1,8 +1,9 @@
-#include <stdio.h>
-
-int main(void){
-   
-    float income[9];
-    printf("This is a budget calculator");
-    return 0;
-}
+float(income) = input("This is a budget calculator. \n How much do you make per month? \n");
+income = (float);
+float(utils) = input("How much do utilities cost a month? \n");
+utils = (float);
+float(groceries) = input("how much do groceries cost a month \n");
+groceries = (float)
+float(budget) = income -  utils - groceries;
+budget = float
+print(budget)
