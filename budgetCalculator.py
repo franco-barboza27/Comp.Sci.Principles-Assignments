9bdc182dc2e954c39a1987bac02ed53ef96fc201
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-09-15 17:51:53 +0000

    tried to fix budgetCalculator.py (not fixed)

--- budgetCalculator.py
+++ budgetCalculator.py
@@ -1,5 +1,5 @@
-float(income) = input("This is a budget calculator. \n How much do you make per month? \n");
-income = (float);
+income = input("This is a budget calculator. \n How much do you make per month? \n");
+(float) income;
 float(utils) = input("How much do utilities cost a month? \n");
 utils = (float);
 float(groceries) = input("how much do groceries cost a month \n");
