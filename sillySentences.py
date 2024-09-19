825b0632505dc487003d049694cbcb7fd2383349
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-09-19 23:59:15 +0000

    added a period

--- sillySentences.py
+++ sillySentences.py
@@ -1,4 +1,4 @@
 noun = input("Name an animal or thing: ");
 place = input("Name a place: ");
 verb = input("Name a past tense action: ");
-print("The " + noun + " went to the " + place + " and " + verb + " with santa");
+print("The " + noun + " went to the " + place + " and " + verb + " with santa.");
