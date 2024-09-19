b7ec0c209a55465decd7c366983d1dca84b64638
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-09-19 01:32:31 +0000

    Finished both name decorator in .c and .py

--- nameDecorator.c
+++ nameDecorator.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
-    char name[40];
-    printf("What is your name? \n");
-    fgets(name, sizeof(name), stdin);
-    char editName = ("###", name, "###");
-    printf("Hello %s",editName);
+    char name[40] = "franco";
+    char hashtag[40] = "###";
+    strcat(name , "###");
+    strcat(hashtag , name);
+    printf("hello %s \n", hashtag);
 return 0;
 }
