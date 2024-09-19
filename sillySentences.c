2a0339733cda057ed37f7621429b73c5271cf38d
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-09-19 23:57:39 +0000

    finished the silly sentences in c and python

--- sillySentences.c
+++ sillySentences.c
@@ -5,17 +5,19 @@ int main(void){
     char animal[20];
     char place[20];
     char verb[20];
-    char sentence[500] = "The";
-    printf("Name an animal: ");
+    char sentence[500] = "The ";
+    printf("Name an animal or thing: ");
     scanf("%s", animal);
-    printf("name a place: ")
-    fgets(place, sizeof(place), stdin);
-    printf("name an animal or thing. \n");
-    fgets(animal, sizeof(animal), stdin);
-    printf("name a place. \n");
-    fgets(place, sizeof(place), stdin);
-    printf("name a verb(action). \n");
-    fgets(verb, sizeof(verb), stdin);
-    printf("The %s walked to the %s and did %s with his friend jared the angry turtle. \n", animal, place, verb);
+    printf("name a place: ");
+    scanf("%s", place);
+    printf("name a past tense verb(action). \n");
+    scanf("%s", verb);
+    strcat(sentence, animal);
+    strcat(sentence, " went to the ");
+    strcat(sentence, place);
+    strcat(sentence, " and ");
+    strcat(sentence, verb);
+    strcat(sentence, " with his friend, the angry turtle. ");
+    printf("%s", sentence);
 return 0;
 }
