5d72d061371b4e86111aecea3eb21d74d281e3fb
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-10-01 23:00:08 +0000

    fixed oldEnough.c (it needed a ";")

--- oldEnough.c
+++ oldEnough.c
@@ -2,7 +2,7 @@
 
 int main(void){
     int age;
-    printf("How old are you?");
+    printf("How old are you?:");
     scanf("%d", &age);
     if (age >= 21){
         printf("You are old enough to drink.");
@@ -14,8 +14,8 @@ int main(void){
         printf("You are old enough to get a learners permit");
     }else if (age >= 5){
         printf("You are old enough to go to school.");
-    }else if (age < 5);{
+    }else if (age < 5){
         printf("You are not old enough to go to school, can you even read this?");
-    }
+    };
 return 0;
 }
