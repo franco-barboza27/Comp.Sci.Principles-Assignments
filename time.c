8d0fe72ef600310e9f0f9a0b146a8986767325cf
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-10-02 19:43:53 +0000

    Participated in the code with me finished time.c

---
+++ time.c
@@ -0,0 +1,23 @@
+/* location example */
+#include <stdio.h>
+#include <time.h>
+int hour;
+int main ()
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+
+    time(&rawtime);
+    timeinfo = localtime (&rawtime);
+    printf("Current local time and date: %s", asctime(timeinfo));
+    printf("What hour is it in military time for you? \n");
+    scanf("%d", &hour);
+    if (hour < 12){
+        printf("Good Morning \n");
+    }else if (hour < 18){
+        printf("Good Afternoon \n");
+    }else{
+        printf("Good Evening \n");
+    };
+return 0;
+}
---
+++ time.py
@@ -0,0 +1,7 @@
+import datetime
+
+currentime = datetime.datetime.now()
+
+print(currentime.hour)
+
+if(time < 12)
