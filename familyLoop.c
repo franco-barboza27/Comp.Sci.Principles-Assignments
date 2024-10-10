4ae991d01a59a85b4c59a6967d1363403b1cf206
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-10-10 22:22:39 +0000

    finished family loop, finished fizzbuzz (.c)

--- familyLoop.c
+++ familyLoop.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 
-char sibs[5][20] = {"franco", "ashton", "gage", "melian", "leo"};
+char sibs[5][30] = {"franco", "ashton", "gage", "melian", "leo"};
 int i;
 int main (){
-    for(i==0;i<=5;i==1){
-        i+=1;
-    }
-    if (sibs == "leo"){
-        printf("%s", sibs[i]);
-        printf(" the also REALLY annoying one, but a lot cuter so he passes. \n. \n");
-    }else if (sibs == "ashton"){
-        printf("%s", sibs[i]);
-        printf(" the funny one. \n");
-    }else if (sibs == "gage"){
-        printf("%s", sibs[i]);
-        printf(" the annoying but not super annoying one. \n");
-    }else if (sibs == "melian"){
-        printf("%s", sibs[i]);
-        printf(" the REALLY annoying one, he's lucky he's only 4. \n");
-    }else{
-        printf("%s", sibs[i]);
-        printf(" the oldest one. \n");
+    for(i==0;i<=5;i++){
+        if (strcmp(sibs[i] , "leo")==0){
+            printf("%s", sibs[i]);
+            printf(" the also REALLY annoying one, but a lot cuter so he passes. \n. \n");
+
+        }else if (strcmp(sibs[i] , "melian")==0){
+            printf("%s", sibs[i]);
+            printf(" the REALLY annoying one, he's lucky he's only 4. \n");
+
+        }else if (strcmp(sibs[i] , "gage")==0){
+            printf("%s", sibs[i]);
+            printf(" the annoying but not super annoying one. \n");
+
+        }else if (strcmp(sibs[i] , "ashton")==0){
+            printf("%s", sibs[i]);
+            printf(" the funny one. \n");
+
+        }else if (strcmp(sibs[i] , "leo")){
+            printf("%s", sibs[i]);
+            printf(" the oldest one. \n");
+        }else{
+            return 0;
+        }
     }
 return 0;
 }
--- fizzbuzz.c
+++ fizzbuzz.c
@@ -9,24 +9,23 @@ char fixer[50];
 
 int main(){
     //add input
-    printf("Give me a number");
+    printf("Give me a number :");
     scanf("%d", &usr);
     printf("Give me a short word NOW!!!!!!");
-    scanf("%s", &one);
+    scanf("%s", one);
     printf("Give me a another short word :)");
-    scanf("%s", &two);
-    strcat("%s", &two);
+    scanf("%s", two);
     strcat(fixer, one);
     strcat(fixer, two);
     //loop that counts to input
     for(i=1;i<=usr;i++){
-        //replace #'s divisible by 3 and 5 with "fizzbuzz"
+        //replace #'s divisible by 3 and 5 with input
         if(i%3==0 && i%5==0){
-            printf("%s\n", one);
-        //replace #'s divisible by 3 with "fizz"
+            printf("%s\n", fixer);
+        //replace #'s divisible by 3 with input
         }else if(i%3==0){
             printf("%s\n", one);
-        //replace #'s divisible by 5 with "fizz"
+        //replace #'s divisible by 5 with input
         }else if(i%5==0){
             printf("%s\n", two);
     //print the number
