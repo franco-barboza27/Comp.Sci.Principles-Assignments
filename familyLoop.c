a08612b11e553655f7a89dbc4a7af3d58ca90e0c
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-10-10 17:49:44 +0000

    worked on familyloop.c

--- familyLoop.c
+++ familyLoop.c
@@ -3,24 +3,24 @@
 char sibs[5][20] = {"franco", "ashton", "gage", "melian", "leo"};
 int i;
 int main (){
-    while(i<5){
-        if (sibs == "franco"){
-            printf("%s", sibs[i]);
-            printf(" the oldest one. \n");
-        }else if (sibs == "ashton"){
-            printf("%s", sibs[i]);
-            printf(" the funny one. \n");
-        }else if (sibs == "gage"){
-            printf("%s", sibs[i]);
-            printf(" the annoying but not super annoying one. \n");
-        }else if (sibs == "melian"){
-            printf("$s", sibs[i]);
-            printf(" the REALLY annoying one, he's lucky he's only 4. \n");
-        }else{
-            printf("%s", sibs[i]);
-            printf(" the also REALLY annoying one, but a lot cuter so he passes. \n");
-        }
+    for(i==0;i<=5;i==1){
         i+=1;
+    }
+    if (sibs == "leo"){
+        printf("%s", sibs[i]);
+        printf(" the also REALLY annoying one, but a lot cuter so he passes. \n. \n");
+    }else if (sibs == "ashton"){
+        printf("%s", sibs[i]);
+        printf(" the funny one. \n");
+    }else if (sibs == "gage"){
+        printf("%s", sibs[i]);
+        printf(" the annoying but not super annoying one. \n");
+    }else if (sibs == "melian"){
+        printf("%s", sibs[i]);
+        printf(" the REALLY annoying one, he's lucky he's only 4. \n");
+    }else{
+        printf("%s", sibs[i]);
+        printf(" the oldest one. \n");
     }
 return 0;
 }
