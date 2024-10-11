4e9109ffbbc82564be326bf60dde68c40c3f0e80
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-10-11 19:45:17 +0000

    did fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,20 @@
+#ask for a number, word1 and word2
+i = 0
+num = int(input("Give me a number. \n"))
+fword = (input("Give me a word.\n"))
+sword = (input("Please give me another word. \n"))
+#replace numbers divisible by 5 and 3 with input1 + input2
+while i < num:
+    if i%3==0 and i%5==0:
+        print(fword + sword)
+    
+#replace numbers divisible by 3 with input1
+    elif i%3==0:
+        print(fword)
+#replace numbers divisible by 5 with input2
+    elif i%5==0:
+        print(sword)
+#count to input
+    else:
+        print(i)
+    i+=1
