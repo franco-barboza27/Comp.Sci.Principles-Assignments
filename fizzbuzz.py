972961d140d396aff3ef8852d7fc6f930ca02c25
franco-barboza27 <franco.barboza@ucas-edu.net>
2024-10-11 23:51:17 +0000

    made FB.py count to actually the input instead of the number right before, (<= instead of <)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 #ask for a number, word1 and word2
-i = 0
+i = 1
 num = int(input("Give me a number. \n"))
 fword = (input("Give me a word.\n"))
 sword = (input("Please give me another word. \n"))
 #replace numbers divisible by 5 and 3 with input1 + input2
-while i < num:
+while i <= num:
     if i%3==0 and i%5==0:
         print(fword + sword)
     
